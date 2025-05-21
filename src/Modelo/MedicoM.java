ded2466dd533c591251ad4f4bd4617fb9c4a3d6f
Arthur Fernandes <arthur.castanheira04@hotmail.com>
2025-05-21 12:43:03 -0400

    Medico M Atualizado

--- src/Modelo/MedicoM.java
+++ src/Modelo/MedicoM.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author arthu
- */
-public class MedicoM {
+public class MedicoM extends DadosPessoaisM{  
+    private String CRM;
+    private String Especialidade;
+    private float Valor;
+    private int NumConsulta;
+    private String Data;
+
+    public MedicoM(String CRM, String Especialidade, float Valor, int NumConsulta, String Data, String Nome, String CPF, int Idade, String Sexo, String DataNascimento, int Telefone, String Email, String Rua, int NumCasa, String bairro, String CEP, String Cidade, String Estado) {
+        super(Nome, CPF, Idade, Sexo, DataNascimento, Telefone, Email, Rua, NumCasa, bairro, CEP, Cidade, Estado);
+        this.CRM = CRM;
+        this.Especialidade = Especialidade;
+        this.Valor = Valor;
+        this.NumConsulta = NumConsulta;
+        this.Data = Data;
+    }
+
+    public String getCRM() {
+        return CRM;
+    }
+
+    public void setCRM(String CRM) {
+        this.CRM = CRM;
+    }
+
+    public String getEspecialidade() {
+        return Especialidade;
+    }
+
+    public void setEspecialidade(String Especialidade) {
+        this.Especialidade = Especialidade;
+    }
+
+    public float getValor() {
+        return Valor;
+    }
+
+    public void setValor(float Valor) {
+        this.Valor = Valor;
+    }
+
+    public int getNumConsulta() {
+        return NumConsulta;
+    }
+
+    public void setNumConsulta(int NumConsulta) {
+        this.NumConsulta = NumConsulta;
+    }
+
+    public String getData() {
+        return Data;
+    }
+
+    public void setData(String Data) {
+        this.Data = Data;
+    }
     
 }
